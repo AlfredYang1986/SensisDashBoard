db10a0a3f96880d4fc58651f5ce4b215fa762b0d
hel <s3395127@student.rmit.edu.au>
2014-04-29 02:05:26 +1000

    Select top ten users - working

--- src/sensis/DBClient/DataBaseHandler.scala
+++ src/sensis/DBClient/DataBaseHandler.scala
@@ -14,6 +14,11 @@ import sensis.DBClient.DAO.DataSource
 import sensis.DBClient.DAO.SplunkDataDAO
 import sensis.DBClient.DAO.User
 import java.text.SimpleDateFormat
+import sensis.DBClient.DAO.SplunkDataDAO
+import scala.collection.immutable.TreeMap
+import sensis.DBClient.DAO.SplunkDataDAO
+import sensis.DBClient.DAO.SplunkDataDAO
+import sensis.DBClient.DAO.SplunkDataDAO
 
 abstract class DataHandlerFacade {
 
@@ -126,29 +131,75 @@ class DataBaseHandler extends DataHandlerFacade {
     ds
   }
 
-  def getTopTenUsers(startDate: String, endDate: String, logSourceName: String):List[SplunkDataDAO] ={
+  def getTopTenUsers(startDate: String, endDate: String, logSourceName: String): List[SplunkDataDAO] = {
 
-    val sd: Date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse(startDate)
-    val start: Long = (new GregorianCalendar(sd.getYear(), sd.getMonth(), sd.getDay()).getTime()).getTime() / (24 * 60 * 60 * 1000)
-    val ed: Date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse(endDate)
-    val end: Long = (new GregorianCalendar(ed.getYear(), ed.getMonth(), ed.getDay()).getTime()).getTime() / (24 * 60 * 60 * 1000)
+    val start: Long = getLongDays(startDate)
+    val end: Long = getLongDays(endDate)
+    var distinctUsers: TreeMap[String, SplunkDataDAO] = TreeMap.empty
 
     var numOfCalls = from db () in "splunkdata" where ("days" $gte start.asInstanceOf[Int], "days" $lte end.asInstanceOf[Int]) select
       {
         x =>
-          new SplunkDataDAO(x.as[String]("key"), x.as[Long]("days"),
-            x.getAsOrElse("getByListingId", 0),
-            x.getAsOrElse("search", 0),
-            x.getAsOrElse("serviceArea", 0),
-            x.getAsOrElse("index/listingsInHeadingInLocality", 0),
-            x.getAsOrElse("singleSearch", 0),
-            x.getAsOrElse("report/appearance", 0),
-            x.getAsOrElse("report/viewDetails", 0),
-            x.getAsOrElse("index/topCategoriesInLocality", 0),
-            x.getAsOrElse("index/topCategoriesInLocality", 0),
-            x.getAsOrElse("index/localitiesInState", 0))
-      }
+          {
+            val userKey = x.as[String]("key")
+            var newDataObj: SplunkDataDAO = null
 
-    numOfCalls
+            if (distinctUsers.keys.exists(_ == userKey)) {
+              val spDao: SplunkDataDAO = distinctUsers(userKey)
+
+              newDataObj = new SplunkDataDAO(userKey, x.as[Long]("days"),
+                (spDao.getByListingId + x.getAsOrElse("getByListingId", 0)),
+                (spDao.search + x.getAsOrElse("search", 0)),
+                (spDao.serviceArea + x.getAsOrElse("serviceArea", 0)),
+                (spDao.listingsInHeadingInLocality + x.getAsOrElse("index/listingsInHeadingInLocality", 0)),
+                (spDao.singleSearch + x.getAsOrElse("singleSearch", 0)),
+                (spDao.appearance + x.getAsOrElse("report/appearance", 0)),
+                (spDao.viewDetails + x.getAsOrElse("report/viewDetails", 0)),
+                (spDao.topCategoriesInLocality + x.getAsOrElse("index/topCategoriesInLocality", 0)),
+                (spDao.topCategoriesInLocality + x.getAsOrElse("index/topCategoriesInLocality", 0)),
+                (spDao.localitiesInState + x.getAsOrElse("index/localitiesInState", 0)))
+              distinctUsers += (userKey -> newDataObj)
+            } else {
+              newDataObj = new SplunkDataDAO(userKey, x.as[Long]("days"),
+                x.getAsOrElse("getByListingId", 0),
+                x.getAsOrElse("search", 0),
+                x.getAsOrElse("serviceArea", 0),
+                x.getAsOrElse("index/listingsInHeadingInLocality", 0),
+                x.getAsOrElse("singleSearch", 0),
+                x.getAsOrElse("report/appearance", 0),
+                x.getAsOrElse("report/viewDetails", 0),
+                x.getAsOrElse("index/topCategoriesInLocality", 0),
+                x.getAsOrElse("index/topCategoriesInLocality", 0),
+                x.getAsOrElse("index/localitiesInState", 0))
+              distinctUsers += (userKey -> newDataObj)
+            }
+            newDataObj
+          }
+      }
+    sortForTopTen(distinctUsers).foreach(println)
+    sortForTopTen(distinctUsers)
+  }
+
+  def getLongDays(date: String): Long = {
+    val d: Date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse(date)
+    val longDays: Long = (new GregorianCalendar(d.getYear(), d.getMonth(), d.getDay()).getTime()).getTime() / (24 * 60 * 60 * 1000)
+    longDays
+  }
+
+  def sortForTopTen(numOfCalls: TreeMap[String, SplunkDataDAO]): List[SplunkDataDAO] = {
+    var counter: Int = 0
+    var arr: Array[SplunkDataDAO] = numOfCalls.values.toArray[SplunkDataDAO]
+
+    for (j <- 0 to (numOfCalls.size)) {
+      counter = 0
+      for (i <- arr) {
+        if ((counter < 33) && (i.getTotalFuncCalls < arr(counter + 1).getTotalFuncCalls)) {
+          arr(counter) = arr(counter + 1)
+          arr(counter + 1) = i
+        }
+        counter = counter + 1
+      }
+    }
+    arr.toList.slice(0, 10)
   }
 }
