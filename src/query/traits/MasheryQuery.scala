35adc815e1ab5c89150a4d965e9ae8fddf024250
hel <s3395127@student.rmit.edu.au>
2014-05-19 04:56:16 +1000

    Mashery user details and user count

--- src/query/traits/MasheryQuery.scala
+++ src/query/traits/MasheryQuery.scala
@@ -1,10 +1,78 @@
 package query.traits
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import scala.collection.immutable.TreeMap
 import scala.util.parsing.json.JSONObject
+import com.mongodb.DBObject
+import com.mongodb.casbah.Imports.mongoQueryStatements
+import com.mongodb.casbah.commons.MongoDBObject
+import query.BaseTimeSpan
+import query.from
 import query.property.SensisQueryElement
+import cache.MasheryDatabaseName
 
 object MasheryQuery extends QueryTraits {
-	def isQueryable(property : String) : Boolean = false
-	def query(b : Int, e : Int, p : SensisQueryElement, r : String*) : JSONObject = ???
-	def queryTops(t : Int, b : Int, e : Int, p : SensisQueryElement, r : String*) : JSONObject  = ???
+  def isQueryable(property: String): Boolean = false
+
+  def query(b: Int, e: Int, p: SensisQueryElement, r: String*): JSONObject = {
+    new JSONObject(queryBase(b, e, p, r.toString))
+  }
+
+  def queryCount(b: Int, e: Int, p: SensisQueryElement, r: String*): JSONObject = {
+    var countMap: TreeMap[String, Int] = TreeMap.empty
+
+    (queryBase(b, e, p, r.toString)).foreach(x => {
+      countMap += (x._1 -> (x._2).size)
+    })
+    new JSONObject(countMap)
+  }
+
+  def queryTops(t: Int, b: Int, e: Int, p: SensisQueryElement, r: String*): JSONObject = ???
+
+  def queryBase(start: Int, end: Int, conditions: SensisQueryElement, req: String*): Map[String, List[SensisQueryElement]] = {
+
+    def queryConditions: DBObject = {
+      val dbObjectBuilder = MongoDBObject.newBuilder
+      for (it <- conditions)
+        dbObjectBuilder += (it.name -> it.get)
+
+      dbObjectBuilder.result
+    }
+
+    def resultConditions(fl: Array[String]): MongoDBObject => SensisQueryElement = {
+      x =>
+        {
+          val re: SensisQueryElement = new SensisQueryElement
+          for (it <- fl)
+            re.insertProperty(it, x.getAsOrElse(it, ""))
+          re
+        }
+    }
+
+    /* Date conversion process for matching Mashery String dates */
+    def getStringDate(day: Int): String = {
+      val cal = Calendar.getInstance()
+      cal.setTime(BaseTimeSpan.base)
+      cal.add(Calendar.DATE, day)
+      new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime())
+    }
+
+    /* Conditions for results */
+    var fl: Array[String] = null
+    if (req.head. equals("*"))
+      fl = Array("display_name", "first_name", "last_name", "username", "email",
+        "area_status", "postal_code", "country_code", "created", "updated")
+    else
+      fl = req.toList.toArray
+
+    /* Data retrieval */
+    var userMap: TreeMap[String, List[SensisQueryElement]] = TreeMap.empty
+    for (i <- start to end) {
+      val tmpData = from db () in MasheryDatabaseName.mashery_data where (("created" $regex getStringDate(i)), queryConditions) select resultConditions(fl)
+      userMap += (i.toString -> tmpData.toList)
+
+    }
+    userMap
+  }
 }
