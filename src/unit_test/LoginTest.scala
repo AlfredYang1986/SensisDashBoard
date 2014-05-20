e4afdac4579f02363b5b95b3679f38ed7b218fec
Dangodaisuki <s3391133@student.rmit.com.au>
2014-05-20 17:48:22 +1000

    ddd

--- src/unit_test/LoginTest.scala
+++ src/unit_test/LoginTest.scala
@@ -2,13 +2,15 @@ package unit_test
 
 import login.UserLogin
 
-object loginTes extends App {
+object loginTest extends App {
 //	println(UserLogin.authorization("Pete", "8888"))
 //	println(UserLogin.authorization("Pete", "123456"))
 //	println(UserLogin.changePassword("Pete", "123456"))
 //	println(UserLogin.authorization("Pete", "123456"))
-	println(UserLogin.addAdmin("Alfred", "123456"))
-	println(UserLogin.authorization("Alfred", "123456"))
-	println(UserLogin.deleteAdmin("Alfred"))
-	println(UserLogin.authorization("Alfred", "123456"))
-}
+	//println(UserLogin.addAdmin("Alfred", "123456"))
+	println(UserLogin.authorization("Tony", "123"))
+	//println(UserLogin.deleteAdmin("Alfred"))
+	//println(UserLogin.authorization("Alfred", "123456"))
+	
+	UserLogin.addAdmin("Alfred", "123456")
+} 
