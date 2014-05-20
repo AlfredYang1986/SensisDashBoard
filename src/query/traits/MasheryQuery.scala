972f57fda3aa67a3da1942d335a59e7e934a70e5
hel <s3395127@student.rmit.edu.au>
2014-05-20 22:39:16 +1000

    Sample QL Trend, Hot and Cold Merge remote-tracking branch 'origin/master'

--- src/query/traits/MasheryQuery.scala
+++ src/query/traits/MasheryQuery.scala
@@ -11,6 +11,7 @@ import query.BaseTimeSpan
 import query.from
 import query.property.SensisQueryElement
 import cache.MasheryDatabaseName
+import query.IQueryable
 
 object MasheryQuery extends QueryTraits {
   def isQueryable(property: String): Boolean = false
@@ -29,6 +30,10 @@ object MasheryQuery extends QueryTraits {
   }
 
   def queryTops(t: Int, b: Int, e: Int, p: SensisQueryElement, r: String*): JSONObject = ???
+
+  def queryTopsWithQueryable(t: Int, b: Int, e: Int, p: SensisQueryElement, r: String*): IQueryable[SensisQueryElement] = ???
+
+  def queryWithQueryable(b: Int, e: Int, p: SensisQueryElement, r: String*): IQueryable[SensisQueryElement] = ???
 
   def queryBase(start: Int, end: Int, conditions: SensisQueryElement, req: Array[String]): Map[String, List[SensisQueryElement]] = {
 
@@ -60,7 +65,7 @@ object MasheryQuery extends QueryTraits {
 
     /* Conditions for results */
     var fl: Array[String] = null
-    if (req.head. equals("*"))
+    if (req.head.equals("*"))
       fl = Array("display_name", "first_name", "last_name", "username", "email",
         "area_status", "postal_code", "country_code", "created", "updated")
     else
@@ -75,7 +80,5 @@ object MasheryQuery extends QueryTraits {
     }
     userMap
   }
-	def queryTopsWithQueryable(t : Int, b : Int, e : Int, p : SensisQueryElement, r : String*) : IQueryable[SensisQueryElement] = ???
-def queryWithQueryable(b : Int, e : Int, p : SensisQueryElement, r : String*) : IQueryable[SensisQueryElement] = ???
 
 }
