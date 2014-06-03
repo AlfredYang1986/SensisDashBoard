4005652b72477082bc6943bbeabe70b271472b58
AlfredYang1986 <yangyuanpig@gmail.com>
2014-06-03 18:04:18 +1000

    date to string bugs

--- src/query/property/SensisQueryElement.scala
+++ src/query/property/SensisQueryElement.scala
@@ -38,7 +38,7 @@ class SensisQueryElement {
 	  			val cal = Calendar.getInstance()
 	  			cal.setTime(BaseTimeSpan.base)
 	  			cal.add(Calendar.DATE, arg.get.asInstanceOf[Int])
-	  			mp += "days" -> new SimpleDateFormat("MM/dd/yyyy").format(cal.getTime())
+	  			mp += "days" -> new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime())
 	  		}
 	  	}
 	  	new JSONObject(mp)
