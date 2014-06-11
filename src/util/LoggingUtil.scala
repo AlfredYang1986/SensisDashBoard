a767968d8cbc5465a73430388852acd595845b00
hel <s3395127@student.rmit.edu.au>
2014-06-11 18:16:38 +1000

    Server Logging Config files

---
+++ src/util/LoggingUtil.scala
@@ -0,0 +1,18 @@
+package util
+
+import org.apache.log4j.Logger
+import org.apache.log4j.xml.DOMConfigurator
+
+object LoggingUtil {
+
+  private var logger: Logger = null
+
+  def getInstance(cls: Class[_]) = {
+    if (logger == null)
+      logger = Logger.getLogger(cls)
+    DOMConfigurator.configure("conf\\loggingProperties.xml")
+
+    logger
+  }
+
+}
