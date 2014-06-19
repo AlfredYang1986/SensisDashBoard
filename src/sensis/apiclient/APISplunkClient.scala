7bbb1acc4dfb4f92e99a04abd7934aa83027dd27
AlfredYang1986 <yangyuanpig@gmail.com>
2014-06-19 12:01:54 +1000

    Update APISplunkClient.scala

--- src/sensis/apiclient/APISplunkClient.scala
+++ src/sensis/apiclient/APISplunkClient.scala
@@ -20,7 +20,7 @@ import java.util.Calendar
 
 object SplunkProxy extends SplunkProxyBase {
   
-	def source_type : String = "sourcetype=access_common"
+	def source_type : String = "sourcetype=access_combined"
   	override def getStartDate(duration : String) : Date = duration match {
 		case "test" => date_format.parse("03/31/2014:03:00:00")
 		case _ => super.getStartDate(duration)
